b820e2bf7052bd2e87a141f350a61966e53ab926
David Porter <david@claimdocs.com>
2018-12-04 23:12:43 -0600

    ready for demo day output for NB model

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# dataminer
+*Typical Workflow:*
+
+1. Pre-process the data files (mainly, text pre-processing) to normalize the data (convert the text into suitable input data format needed by your classifier model).
+
+2. Build classifier model (involve feature engineering/representation, designing/implementing the model, Hyper-parameter tuning etc.)
+
+3. Evaluate the performance of the classifier model and report results
+
+
+
+Try to build multiple classifier models and compare them based on their predictive performance, find the best one.
+
+Analyze why they works well or why they does not. Modify models/features/pre-processing steps accordingly and re-evaluate.
+
+The goal is to build best possible version of your sentiment predictor to solve the task.
+
+
+*Evaluation:*
+
+1. Results should be generated via 10-fold cross validation. You can also use stratified cross-validation while generating 10 folds.
+
+2. Compute following metrics by taking average over 10 folds-
+
+
+Accuracy and avg. precision, avg. recall and avg. F1 scores for each of the three classes- { positive, negative, neutral } and for each of the two training dataset {data-1, data-2}.
+
+
+3. Make sure the test fold remain "unseen" to the classifier during its training for a "fair" evaluation
+
+
+
+
+*Project 2 Demo Instructions, Time and Place*
+
+
+Hi all,
+The project 2 demo will be held on Thursday (12/06/2018) at SEO Room 809. It will start at 3:30pm and end at 5:00 pm (roughly the same time as the class).
+
+Evaluation Plan:  Assuming that there are around 34 groups coming, we split the groups into two halves and each group will be assigned to one TA. That is, 2 groups will enter in the room and be evaluated simultaneously, each of which will be interacting with one TA. The evaluation is expected to be finished in 90 minutes so please be on time.
+Demo Slots:  We divided the whole duration into 2 slots: Section/Batch - 1 (3:30pm to 4:15pm) and Section / Batch - 2 ( 4:15pm to 5pm). 17 groups at max will be evaluated by BOTH TAs in each slot. You can choose your time slot according to your preference by filling up the following google sheet:
+Fill UP the Google Sheet to Reserve your Slot!
+
+The contact email-ids [ "," separated ] will be used in sending you the dataset.
+
+What You will need to Do in Demo:
+1. Test data will be sent to you 1hr before the start of your corresponding demo slot. There will be two .csv test files: one for Data-1 and another for Data-2. Each row is one test instance [ just like your training data files (Exactly same format!)]. But, class labels will NOT be provided for demo.
+2. You should use the labeled data you already have for training your models (before we send you the test set) and simply apply your BEST trained classifier model on the newly-provided test data. No cross-validation!! Your classifier will just make one pass over all test instances and predict the class labels (-1/0/1).
+
+Important Note (specially for deep learning methods): If you think your model training takes long time, you should pre-train your "best classifier model" and "dump" it before coming for demo. During demo, you just need to load the model for classification and run on test set without any need for re-training !! In other words, we do not expect you to train your model on the spot. What you need to do is to run your trained model and make prediction on test sets in front of us.   
+
+3. Set up everything. And Once set up, come for a demo and run you classifier in front of us on a specified test set .  
+
+4. Final (Important) STEP:  Almost done! Mail BOTH TAs  ( smazum2@uic.edu ,  swang207@uic.edu )  the following files (in a single mail) DURING EVALUATION ON the SPOT ( when asked ).
+
+-- two output files (one for each test set )  [See below for naming convention]
+-- runnable project code with a README (How to setup and run your code) in a zipped folder.
+-----------------------------------------------------------
+NO ZIPPING of output files!  ONLY ZIP your source code.
+MAIL HEADER:   "Project 2 Demo Submission"
+The output files should be named as follows:
+
+"FirstName1_Lastname1_FirstName2_Lastname2_Data-1.txt"
+"FirstName1_Lastname1_FirstName2_Lastname2_Data-2.txt"
+
+Content of each file:   Format: "Review/Text_ID;;Predicted Class Label\n"
+
+Example:
+1345;;1
+2A7B;;-1
+5T68;;0
+45E2;;1
+5A#9;;1
+so on..
+
+-----------------------------------------
+We'll run a script that will read all submissions and directly compute Accuracy, F1 scores for positive, neutral and negative classes. So, STICK to the format. Also, CHECK whether THE NUMBER OF LINES in YOUR OUTPUT PREDICTION FILE is same as the input TEST FILE BEFORE you go for a SUBMIT.
+Acknowledge us if you have any queries!
+Best wishes!
+Sahisnu & Shuai
+#pin
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,21 +8,184 @@ import pandas as pd
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
-from sklearn import naive_bayes
-<<<<<<< HEAD
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import f1_score
-
-=======
+from sklearn import naive_bayes, svm
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics import roc_auc_score, precision_recall_fscore_support
->>>>>>> 2d044970904bebc0eb6752df1a36399161d84d63
+from sklearn.metrics import roc_auc_score, precision_recall_fscore_support, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 
 
+
+# this doesn't work but it doesn't matter anymore since it was only asked for the presentation for nb and they will provide their own evaluation during the demo. But we could use kfold for the writeup as a secondary evaluator to our model.
+def nb_trainNtest_10fold():
+    kf = KFold(n_splits=10)
+    # print(kf)
+    for train_index, test_index in kf.split(X1):
+        model = naive_bayes.MultinomialNB()
+        print(train_index, test_index)
+        print("x1-> ", X1)
+        # print(X1[train_index][1:], y1[train_index][1:])
+        model = model.fit(X1[train_index][1:], y1[train_index][1:])
+        # print(model)
+        pred = model.predict(X1[test_index][1:])
+        actual = np.array(y1[test_index][1:])
+        print(pred,actual)
+
+        # for i,j in zip(pred, actual):
+        #     print(i,j)
+        neutral_count=0
+        positive_count=0
+        negative_count=0
+        neu_total=0
+        p_total=0
+        neg_total=0
+        for i in range (len(actual)):
+            p = pred[i]
+            a = actual[i]
+            print(p,a)
+
+        ##count totals for each case
+            if a == '0':
+                neu_total+=1
+            elif a == '1':
+                p_total+=1
+            elif a == '-1':
+                neg_total+=1
+
+        ##count total predicted correctly for each case
+            if p == '0' and p == a:
+                neutral_count = neutral_count + 1
+            if p == '1' and p == a:
+                positive_count = positive_count + 1
+            if p == '-1' and p == a:
+                negative_count = negative_count + 1
+
+        print("negative=", negative_count/neg_total)
+        print("neutral=", neutral_count/neu_total)
+        print("positive", positive_count/p_total)
+
+    print("Naive Bayes precision, recall, fscore")
+
+    print(precision_recall_fscore_support(actual, pred, average='weighted'))
+
+
+# this simply used as a measure of evaluation for our NB model by printing pos, neu, and neg results. need to improve preprocessing for low numbers in each category.
+def nb_trainNtest():
+    model2 = naive_bayes.MultinomialNB()
+    model2 = model2.fit(X_train, y_train)
+    pred = model2.predict(X_test)
+    actual = np.array(y_test)
+
+
+    neutral_count=0
+    positive_count=0
+    negative_count=0
+    neu_total=0
+    p_total=0
+    neg_total=0
+    precision_neu_total=0
+    precision_p_total=0
+    precision_neg_total=0
+
+    for i in range(len(actual)):
+        p = pred[i]
+        # print(p)
+        a = actual[i]
+        # print(p,a)
+
+    ##count totals for each case
+        if a == '0':
+            neu_total+=1
+        elif a == '1':
+            p_total+=1
+        elif a == '-1':
+            neg_total+=1
+
+        if p == '0':
+            precision_neu_total+=1
+        elif p == '1':
+            precision_p_total+=1
+        elif p == '-1':
+            precision_neg_total+=1
+
+
+    ##count total predicted correctly for each case
+        if p == '0' and p == a:
+            neutral_count = neutral_count + 1
+        if p == '1' and p == a:
+            positive_count = positive_count + 1
+        if p == '-1' and p == a:
+            negative_count = negative_count + 1
+
+
+    print("recall_negative=", negative_count/neg_total)
+    print("recall_neutral=", neutral_count/neu_total)
+    print("recall_positive=", positive_count/p_total)
+
+    print("precision_negative=", negative_count/precision_neg_total)
+    print("precision_neutral=", neutral_count/precision_neu_total)
+    print("precision_positive=", positive_count/precision_p_total)
+
+    print("F-score_negative=", 2*((negative_count/precision_neg_total)*(negative_count/neg_total))/((negative_count/precision_neg_total)+(negative_count/neg_total)))
+    print("F-score_neutral=", 2*((neutral_count/precision_neu_total)*(neutral_count/neu_total))/((neutral_count/precision_neu_total)+(neutral_count/neu_total)))
+    print("F-score_positive=", 2*((positive_count/precision_p_total)*(positive_count/p_total))/((positive_count/precision_p_total)+(positive_count/p_total)))
+
+
+    print("Naive Bayes accuracy:")
+    # print(count/len(pred))
+    print("Naive Bayes precision, recall, fscore")
+    print(precision_recall_fscore_support(actual, pred, average='weighted'))
+
+
+def nb_for_demo(demo_file):
+     # train model per usual with previously provided data
+    model2 = naive_bayes.MultinomialNB()
+    model2 = model2.fit(X_train, y_train)
+    # dont really need these two lines
+    # print(len(X_test))
+    pred = model2.predict(X_test)
+    actual = np.array(y_test)
+
+
+# **PREPROCESS DEMO FILE**
+    df = pd.read_csv(demo_file, names=['example_id', 'text', 'aspect_term', 'term_location', 'classy'])
+    # remove header
+    df = df.drop([0], axis=0)
+    # vectorizes the text column (bag of words)
+    df_vectorized = vectorizer.transform(df.text)
+    # get predictions
+    demo_pred = model2.predict(df_vectorized)
+
+    f = open("david_porter_raquib_yousuf.txt", "w")
+    example_ids = df['example_id'].values
+    print(len(demo_pred))
+    for i in range(len(demo_pred)):
+        f.write(example_ids[i]+';;'+demo_pred[i]+'\n')
+    f.close()
+
+
+
+
+
+# svm try
+def svm_trainNtest():
+    svm_model = svm.SVC(gamma='scale')
+    svm_model.fit(X_train, y_train)
+    svmpred = svm_model.predict(X_test)
+    count = 0
+    for i in range(len(actual)):
+        if svmpred[i] == actual[i]:
+            count = count + 1
+    print("svm accuracy:")
+    print(count / len(svmpred))
+    print("svm precision, recall, fscore")
+    print(precision_recall_fscore_support(actual, svmpred, average='weighted'))
+
+
+
+
+# **PREPROCESSING**
 
 #read csv and label columns since default adds a space before some names
 df1 = pd.read_csv('training_data/data-1_train.csv', names=['example_id', 'text', 'aspect_term', 'term_location', 'classy'])
@@ -39,147 +202,19 @@ stopset = set(stopwords.words('english'))
 stopset.add('[comma]')
 vectorizer = CountVectorizer(stop_words=stopset)
 X = vectorizer.fit_transform(dataframe.text)
-X1 = vectorizer.fit_transform(df1.text)
-X2 = vectorizer.fit_transform(df2.text)
+print(X)
+# X1 = vectorizer.fit_transform(df1.text)
+# X2 = vectorizer.fit_transform(df2.text)
 y = dataframe.classy
-y1 = df1.classy
-y2 = df2.classy
+# y1 = df1.classy
+# y2 = df2.classy
 
-# model = naive_bayes.MultinomialNB()
+# Training on both provided data sets (X, y)... why not?
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+# nb_trainNtest()
+#
+# NEED to change demo_file string on demo day
+demo_filename = 'training_data/data-1_train.csv'
 
-
-kf = KFold(n_splits=10)
-print(kf)
-for train_index, test_index in kf.split(X1):
-    model = naive_bayes.MultinomialNB()
-
-    print(X1[train_index][1:], y1[train_index][1:])
-    model = model.fit(X1[train_index][1:], y1[train_index][1:])
-    print(model)
-    pred = model.predict(X1[test_index][1:])
-    actual = np.array(y1[test_index][1:])
-    # for i,j in zip(pred, actual):
-    #     print(i,j)
-    neutral_count=0
-    positive_count=0
-    negative_count=0
-    neu_total=0
-    p_total=0
-    neg_total=0
-    for i in range (len(actual)):
-        p = pred[i]
-        a = actual[i]
-        # print(p,a)
-
-    ##count totals for each case
-        if a == '0':
-            neu_total+=1
-        elif a == '1':
-            p_total+=1
-        elif a == '-1':
-            neg_total+=1
-
-    ##count total predicted correctly for each case
-        if p == '0' and p == a:
-            neutral_count = neutral_count + 1
-        if p == '1' and p == a:
-            positive_count = positive_count + 1
-        if p == '-1' and p == a:
-            negative_count = negative_count + 1
-
-    print("negative=", negative_count/neg_total)
-    print("neutral=", neutral_count/neu_total)
-    print("positive", positive_count/p_total)
-
-
-model2 = naive_bayes.MultinomialNB()
-model2 = model2.fit(X_train, y_train)
-pred = model2.predict(X_test)
-actual = np.array(y_test)
-print(actual)
-# accuracy = cross_val_score(model, X1, y1, cv=10, scoring='accuracy')
-# print("cv=10 accuracy = ", accuracy)
-# recall = cross_val_score(model, X, y, cv=10, scoring='recall')
-
-
-# labels=[-1,0,1]
-# print(f1_score(pred, actual, average='micro', labels=labels))
-# f1 = cross_val_score(model, X, y, cv=10, scoring='f1')
-#produces same results as F1_score method but doesn't have "elementwise comparison warning."
-# f1score = model.score(X_test, y_test)
-# print("F1_score = ",f1score)
-
-
-neutral_count=0
-positive_count=0
-negative_count=0
-neu_total=0
-p_total=0
-neg_total=0
-precision_neu_total=0
-precision_p_total=0
-precision_neg_total=0
-
-for i in range(len(actual)):
-    p = pred[i]
-    print(p)
-    a = actual[i]
-    # print(p,a)
-
-##count totals for each case
-    if a == '0':
-        neu_total+=1
-    elif a == '1':
-        p_total+=1
-    elif a == '-1':
-        neg_total+=1
-
-    if p == '0':
-        precision_neu_total+=1
-    elif p == '1':
-        precision_p_total+=1
-    elif p == '-1':
-        precision_neg_total+=1
-
-
-##count total predicted correctly for each case
-    if p == '0' and p == a:
-        neutral_count = neutral_count + 1
-    if p == '1' and p == a:
-        positive_count = positive_count + 1
-    if p == '-1' and p == a:
-        negative_count = negative_count + 1
-
-
-print("recall_negative=", negative_count/neg_total)
-print("recall_neutral=", neutral_count/neu_total)
-print("recall_positive=", positive_count/p_total)
-
-print("precision_negative=", negative_count/precision_neg_total)
-print("precision_neutral=", neutral_count/precision_neu_total)
-print("precision_positive=", positive_count/precision_p_total)
-
-print("F-score_negative=", 2*((negative_count/precision_neg_total)*(negative_count/neg_total))/((negative_count/precision_neg_total)+(negative_count/neg_total)))
-print("F-score_neutral=", 2*((neutral_count/precision_neu_total)*(neutral_count/neu_total))/((neutral_count/precision_neu_total)+(neutral_count/neu_total)))
-print("F-score_positive=", 2*((positive_count/precision_p_total)*(positive_count/p_total))/((positive_count/precision_p_total)+(positive_count/p_total)))
-
-
-print("Naive Bayes accuracy:")
-print(count/len(pred))
-print("Naive Bayes precision, recall, fscore")
-print(precision_recall_fscore_support(actual, pred, average='weighted'))
-
-
-# svm try
-svm_model = svm.SVC(gamma='scale')
-svm_model.fit(X_train, y_train)
-svmpred = svm_model.predict(X_test)
-count = 0
-for i in range(len(actual)):
-    if svmpred[i] == actual[i]:
-        count = count + 1
-print("svm accuracy:")
-print(count / len(svmpred))
-print("svm precision, recall, fscore")
-print(precision_recall_fscore_support(actual, svmpred, average='weighted'))
+nb_for_demo(demo_filename)
+# svm_trainNtest()
